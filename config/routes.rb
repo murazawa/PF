92bedb3d9718e7a965743cb62cd7b72315fec013
murazawa <ahahanoha.m0115@gmail.com>
2021-09-04 16:16:47 +0000

    ログイン機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -10,19 +10,20 @@ Rails.application.routes.draw do
   }
   
   resources :players, only: [:show, :edit, :update]
-  resources :games, only: [:index] do
+  resources :games, only: [:index]
+  
+  # タイムアタックモード
+  get 'games/syllabaries' # 50音モード
+  get 'games/greats_men' # 偉人の名言モード
+  get 'games/standards' # スタンダードモード
+  
+  # フェードアウトモード
+  get 'games/fade_outs' # FOモード
+  
+  # クリエイトモード
+  get 'games/creates' # クリエイトモード
     
-    # タイムアタックモード
-    get '/syllabaries' => 'games#syllabaries' # 50音モード
-    get '/greats_men' => 'games#greats_men' # 偉人の名言モード
-    get '/standards' => 'games#standards' # スタンダードモード
-    
-    # フェードアウトモード
-    get '/fade_outs' => 'games#fade_outs' # FOモード
-    
-    # クリエイトモード
-    get '/creates' => 'games#creates' # クリエイトモード
-  end  
+
 
   resources :theme_groups, only: [:index, :new, :edit, :update, :destroy]
   resources :theme_words, only: [:index, :new, :edit, :update, :destroy]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_04_092116) do
+ActiveRecord::Schema.define(version: 2021_09_04_144046) do
 
   create_table "creates", force: :cascade do |t|
     t.string "create_result"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2021_09_04_092116) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "player_name"
+    t.string "image_id"
     t.index ["email"], name: "index_players_on_email", unique: true
     t.index ["reset_password_token"], name: "index_players_on_reset_password_token", unique: true
   end
