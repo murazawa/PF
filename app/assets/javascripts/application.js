61915d384ca139ba08f29b6d888300f57c6051bc
murazawa <ahahanoha.m0115@gmail.com>
2021-09-11 12:05:04 +0000

    update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,11 @@
 //= require jquery
 //= require jquery_ujs
 
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
